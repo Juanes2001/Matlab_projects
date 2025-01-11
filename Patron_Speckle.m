b80a9394ddba6258086b6f17ba90c8bcda40a1f6
Juanes2001 <111066079+Juanes2001@users.noreply.github.com>
2025-01-11 00:23:38 -0500

    h

---
+++ Patron_Speckle.m
@@ -0,0 +1,10 @@
+%%%% Transformada de Fourier usando lentes %%%%%%%
+%% Supondremos que la transformada numerica hallada corresponde a la que se obtiene
+% al usar una lente en planos cofocales
+
+
+resolucion = 500;
+dx = 1E-6;
+dy = 1E-6;
+
+lMask_x = dx*resolucion;
