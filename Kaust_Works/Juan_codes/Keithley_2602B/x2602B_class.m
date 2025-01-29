627b72caab6e7a3917bfd06c7a3cb9bf3a83fbb2
Juanes2001 <jrodriguezoc@unal.edu.co>
2025-01-29 08:55:44 +0300

    c

--- Kaust_Works/Juan_codes/Keithley_2602B/x2602B_class.m
+++ Kaust_Works/Juan_codes/Keithley_2602B/x2602B_class.m
@@ -29,21 +29,37 @@ classdef x2602B_class
         GPIB_address
         Interface_index
 
-
-
         %%% Properties of the control
-        voltage_on_off
-        current_on_off
+        voltage_on_off % single value true or false
+        current_on_off % single value true or false
         channelA_on_off %Array ['A',ON_OFF]
         channelB_on_off %Array ['B',ON_OFF]
+        voltage_value % Array [source, Measure]
+        current_value % Array [source, Measure]
+        voltUnits     % Array [source, Measure] BOTH STRINGS
+        currentUnits  % Array [source, Measure] BOTH STRINGS
+        Mode          %Source/Measure
 
     end    
     
     
     methods
-        %% CONSTRUCTOR
-        function obj = x2602B_class(InBuffSize, outBuffSize, Timeout, vend ,aDDr ...
-                                    ,interIndex)
+        %% CONSTRUCTOR for communication parameters and testing
+        function obj = x2602B_class(InBuffSize, ...
+                                    outBuffSize, ...
+                                    Timeout, ...
+                                    vend , ...
+                                    aDDr, ...
+                                    interIndex, ...
+                                    voltlogic, ...
+                                    currlogic, ...
+                                    chnALogic, ...
+                                    chnBLogic, ...
+                                    voltageVal, ...
+                                    currentVal, ...
+                                    vUnits, ...
+                                    cUnits, ...
+                                    mode)
             %x2602B_class  constructor, just set the principal and
             %important parameters to set due correct communication.
             obj.InputBufferSize     = InBuffSize; %% Data to be received
@@ -52,12 +68,52 @@ classdef x2602B_class
             obj.Vendor              = vend;       %% Vendor from where is the visa drivers
             obj.GPIB_address        = aDDr;       %% Address of the GPIB com
             obj.Interface_index     = interIndex; %% Number of devices communicating
-    
+            
+
+            obj.voltage_on_off  = voltlogic;
+            obj.current_on_off  = currlogic;
+            obj.channelA_on_off = chnALogic;
+            obj.channelB_on_off = chnBLogic;
+            obj.voltage_value   = voltageVal;
+            obj.current_value   = currentVal;
+            obj.voltUnits       = vUnits;
+            obj.currentUnits    = cUnits;
+            obj.Mode            = mode;
+            
         end
 
         %end constructor function
+
+
+        %% REFREST TESTING PARAMETERS
+        function obj = refresh(obj, ...
+                         voltlogic, ...
+                         currlogic, ...
+                         chnALogic, ...
+                         chnBLogic, ...
+                         voltageVal, ...
+                         currentVal, ...
+                         vUnits, ...
+                         cUnits, ...
+                         mode)
+            % we will only refresh the testing parameters it will change 
+            %further later when we want to use some of the functions bellow 
+            obj.voltage_on_off  = voltlogic;
+            obj.current_on_off  = currlogic;
+            obj.channelA_on_off = chnALogic;
+            obj.channelB_on_off = chnBLogic;
+            obj.voltage_value   = voltageVal;
+            obj.current_value   = currentVal;
+            obj.voltUnits       = vUnits;
+            obj.currentUnits    = cUnits;
+            obj.Mode            = mode;
+
+            
+        end
+
+
         %% SET UP AND INITIATION
-        function visa_obj = init(obj)
+        function [logic,ident] = init(obj)
             % with init() we want to initiate the device properly 
             
             instr  = instrfind;% we have to be sure we close the every single opened intrument
@@ -73,14 +129,24 @@ classdef x2602B_class
 
             fopen(visa_obj);
             % With this we set the device on the remote mode
+            pause(1)
+            if ~isempty(iDN(visa_obj))
+                logic = true;
+                ident = iDN(visa_obj); 
+            else
+                logic = false;
+                ident = "Communcation Error";
+            end
         end
 
 
         %% SHUT DOWN COMMUNICATON
 
-        function shutDown(obj)
+        function logic = shutDown(obj)
             % Close and delete the GPIB object
             fclose(Keithley);
+            pause(1);
+            logic = true;
             delete(Keithley);
             clear Keithley;
         end
@@ -123,12 +189,12 @@ classdef x2602B_class
                     fprintf(visa_obj , sprintf("smu%s.source.func = smu%s.OUTPUT_DC%s", ...
                                                 obj.channelA_on_off(1), ...
                                                 obj.channelA_on_off(1), ...
-                                                'VOLTS'));
+                                                'AMPS'));
                 else
                     fprintf(visa_obj , sprintf("smu%s.source.func = smu%s.OUTPUT_DC%s", ...
                                                 obj.channelB_on_off(1), ...
                                                 obj.channelB_on_off(1), ...
-                                                'VOLTS'));
+                                                'AMPS'));
                 end    
             else
                 %%%
@@ -137,25 +203,48 @@ classdef x2602B_class
 
         end
 
-        %% ENABLE/DISABLE
-        function on_offSource(obj,visa_obj)
+        %% ENABLE/DISABLE channel A
+        function obj = Toggle_A(obj,visa_obj)
             % We enable and disable the source, whatever channel and kind
-            % of source the user is using
-                
+            % of source the user is using and defined in the properties
+                 
+            obj.channelA_on_off(2) = ~obj.channelA_on_off(2); % We toggle the state
+
             if obj.channelA_on_off(2) 
                 fprintf(visa_obj, sprintf("smu%s.source.output = smu%s.OUTPUT_ON", ...
                                             obj.channelA_on_off(1), ...
                                             obj.channelA_on_off(1) ));
-            elseif   
+            else   
                 fprintf(visa_obj, sprintf("smu%s.source.output = smu%s.OUTPUT_OFF", ...
                                             obj.channelA_on_off(1), ...
                                             obj.channelA_on_off(1) ));
-
             end
+            
 
         end
+        
+        %% ENABLE/DISABLE channel B
+        function obj = Toggle_B(obj,visa_obj)
+            % We enable and disable the source, whatever channel and kind
+            % of source the user is using and defined in the properties
+                 
+            obj.channelB_on_off(2) = ~obj.channelB_on_off(2); % We toggle the state
+
+            if obj.channelB_on_off(2) 
+                fprintf(visa_obj, sprintf("smu%s.source.output = smu%s.OUTPUT_ON", ...
+                                            obj.channelB_on_off(1), ...
+                                            obj.channelB_on_off(1) ));
+            else   
+                fprintf(visa_obj, sprintf("smu%s.source.output = smu%s.OUTPUT_OFF", ...
+                                            obj.channelB_on_off(1), ...
+                                            obj.channelB_on_off(1) ));
+            end
+            
+
+        end
+
         %% SET THE SOURCE LEVEL
-        function src_level(obj,visa_obj,channel, source, value, units)
+        function src_level(obj,visa_obj)
 
             % First we set the string of the source just to know if 
             
