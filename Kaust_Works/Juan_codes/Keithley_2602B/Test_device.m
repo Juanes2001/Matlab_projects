85bf02ee12c1a7a9d6cb68be17f30ba98a4612fb
Juanes2001 <jrodriguezoc@unal.edu.co>
2025-02-02 20:54:50 +0300

    c

--- Kaust_Works/Juan_codes/Keithley_2602B/Test_device.m
+++ Kaust_Works/Juan_codes/Keithley_2602B/Test_device.m
@@ -8,12 +8,6 @@ clear; clc;
  
 Power_meter = x2602B_class("NI", 26, 0);
 
-disp(Power_meter.iDN(Power_meter.Visa_obj));
-Power_meter.set_src_mode(Power_meter.Visa_obj,'A',"current");
-Power_meter.set_src_volt_curr_level(Power_meter.Visa_obj,'A',"current",0.010);
-
-
-disp(Power_meter.get_meas(Power_meter.Visa_obj,'A',"voltage"));
 
 
 
