fc85066d46f0cde6053e77c11eecf93f842717d7
Juanes2001 <jrodriguezoc@unal.edu.co>
2025-02-06 15:21:15 +0300

    c

---
+++ Kaust_Works/Juan_codes/Agilent_E4448A/ESA_E4448A_class.m
@@ -0,0 +1,176 @@
+classdef ESA_E4448A_class < handle
+    % ESA_E4448A_class this class has the basic methods to use the OSA 
+    % reference A6380, for further complex procedures needed for the
+    % research field, feel free to modify the methods as you want and add
+    % more if needed. Communication is done by ETHERNET TCP\IP by formatted
+    % commands sent through, all this class is using its own properties.
+
+
+    
+    %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % HERE ARE ALL THE METHODS DEFINED, INPUTS AND OUTPUTS AND PROPERTIES DESCRIPTION
+    %   The properties are defined like this:
+    %  
+    % 
+    % %%%%%%%%%%%%%%%%%%%%%% METHODS DESCRIPTION %%%%%%%%%%%%%%%%%%%%%%%%%% 
+    % 
+    %%%%%% CONSTRUCTOR %%%
+    %   
+    %  
+    %   
+    %%%%%%%%%%%%%%%%%%%%
+    % 
+    % 
+    %
+    % 
+    % 
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    properties
+        %%% Properties for communication protocol used GPIB
+        user_name
+        password
+        ip_num
+        port_num
+        timeout
+        TPC_obj
+
+    end    
+    
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    methods
+        %% CONSTRUCTOR for communication parameters and testing
+        function obj = A6380_class(user,password)
+            % A6380_class  constructor, just set the principal and
+            %important parameters to set due correct communication.
+                
+            obj.user_name   = user;
+            obj.password    = password;
+            obj.ip_num      = '10.72.171.64';
+            obj.port_num    = 10001;
+            obj.timeout     = 10;
+
+
+
+
+            %%% We start initiating the Ethernet communication
+
+            instr  = instrfind; % We have to be sure we close every single opened intrument
+            if ~isempty(instr)
+                fclose(instr);
+                delete(instr);
+            end
+
+            % Create a TCP/IP object 't' associated with the OSA
+            obj.TPC_obj = tcpip(obj.ip_num, obj.port_num, 'NetworkRole', 'client');
+            obj.TPC_obj.Timeout =  obj.timeout;
+            
+
+        end % End function
+
+        %end constructor function
+
+
+         %% REFREST TESTING PARAMETERS 
+        %Here will be shown all the refreshing functions which work to
+        %mantain all the info on the properties and not as an input of the
+        %methods written, only for simplicity, so dont expect to use this
+        %functions some time, they are secondary meant to use in the
+        %principal methods.
+
+       function obj = refresh(obj)
+        
+        end% End function
+
+
+        %% INIT COMMUNICATION
+
+        function init(obj)
+            % Using this function you open communication wiith the OSA
+            % Open the connection to the OSA
+            try
+                fopen(obj.TPC_obj);
+                pause(1);
+                disp("Connection successful\n");
+            catch e
+                disp('Failed to connect to the OSA:');
+                disp(e.message);
+            end
+
+            % Check if the connection is open
+            if strcmp(obj.TPC_obj.Status, 'open')
+            
+                fprintf(obj.TPC_obj, "open ""anonymous""");
+                idnResponse = fscanf(obj.TPC_obj);
+                disp(idnResponse);
+            
+                % Send a command to the OSA
+                fprintf(obj.TPC_obj, ' '); % Query the instrument identity
+            
+                % Read the response from the OSA
+                idnResponse = fscanf(obj.TPC_obj);
+                disp('Response from OSA:');
+                disp(idnResponse);
+            end
+            
+        end
+
+
+
+
+
+
+         %% SHUT DOWN COMMUNICATON
+
+        function logic = deleteObj(obj)
+            % Close and delete the GPIB object
+            fclose(obj.TPC_obj);
+            pause(1);
+            logic = true;
+            delete(obj.TPC_obj);
+            clear obj.TPC_obj;
+        end
+
+         %% Identification
+
+         function msg_idn = iDN(obj)
+            % Send a messege of identification
+            fprintf(obj.TPC_obj , '*IDN?'); % We sent the command, then it will send us
+                                             % back the response
+
+            msg_idn = fscanf(obj.TPC_obj);  % We try to read the port anr return the identificator
+         end 
+
+        %% RESET
+        function reset(obj)
+            % Reset the system
+            fprintf(obj.TPC_obj ,'*RST');
+        end
+
+        %% ABORT MEASUREMENTS
+        function abort(obj)
+            % With this function you can abort some measurements ongoing
+            % for any reason needed.
+            fprintf(obj.TPC_obj ,':ABORT');
+        end
+
+
+
+
+
+    end % End of the methods
+
+
+end %End of the class
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
