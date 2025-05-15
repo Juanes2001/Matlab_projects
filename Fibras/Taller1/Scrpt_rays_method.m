b30e32998701eb91022e21162153547f1a1d5b11
Juanes2001 <jrodriguezoc@unal.edu.co>
2025-05-15 00:02:50 -0500

    c

--- Fibras/Taller1/Scrpt_rays_method.m
+++ Fibras/Taller1/Scrpt_rays_method.m
@@ -1,5 +1,5 @@
 %%%%%%%%%%%%%%%%%% MÉTODO DE RAYOS/ SLN USANDO LA RUTINA DE %%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%       NEWTON RAPHSON     %%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%     NEWTON RAPHSON     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 %% La idea es usar el metodo de newton esperando a que nuestra funcion 
@@ -9,9 +9,8 @@
 %  1. Que su valor si se encuentre dentro del rango permitido por la
 %  estructura.
 %  2. Que el cero correspondiente pueda ser encontrado por el metodo de
-%  Newton, por lo que se tiene que cumplir que haya cambio de signo para el
-%  cero correspondiente, condicion para que exista convergencia hacia el
-%  cero deseado.
+%  Newton, condicion para que exista convergencia hacia el
+%  cero deseado de forma rapida y eficiente.
 
 %% Comenzamos escribiendo la funcion de tal forma que variamos el parametro 
 % m para hallar la cantidad de modos en la guía.
@@ -163,6 +162,26 @@ dfTM = @(x) -2*sind(x) - (2/pi)*(1./( ((1.5)^2./((cosd(x)).^2) ) .* (1.5^2 * (si
             sind(x).*cosd(x))./sqrt(1.5^2 * (sind(x)).^2 -1 ) ) );
 
 
+% d2fTE = @(x) -2*cosd(x)-(2/pi)*( ( ( -(1/1.5^2)* (  ((2*sind(x))./(cosd(x)).^2).*(1.5^2*(sind(x)).^2 - 1)  + ...
+%             (2./(cosd(x)).^2).* (1.5^2*sind(x).*cosd(x)) )    )./ ((  (1./(1.5*cosd(x)).^2).*(1.5^2*(sind(x)).^2 - 1 ) + 1  ).^2) ) .* ...
+%              ( (1/1.5) * ( (sind(x)./ (cos(x)).^2 )  .* sqrt(1.5^2 * (sind(x)).^2 - 1) + (1./cosd(x)).*(1.5^2 * ...
+%             sind(x).*cosd(x))./sqrt(1.5^2 * (sind(x)).^2 -1 ) ) )   + (1./( (1./( (1.5)^2 * (cosd(x)).^2) ) .* (1.5^2 * (sind(x)).^2 - 1) + 1 ) ).* ...
+%               (  (1/1.5)* ( (( (cosd(x)).^3 +2*(sind(x)).^2 .*cosd(x) )./ ( (cosd(x)).^4 ) ).* sqrt(1.5^2 * (sind(x)).^2 - 1) + ... 
+%                ( sind(x)./( (cosd(x)).^2 )  ).* ( (1.5^2 * sind(x).*cosd(x))./sqrt(1.5^2 * (sind(x)).^2 -1 ) ) )  +  (1.5^2/1.5) * ( ...
+%                ( sind(x)./( (cosd(x)).^2 )  ).* ( (sind(x).*cosd(x))./sqrt(1.5^2 * (sind(x)).^2 -1 ) ) + (1./cosd(x)).* ...
+%                ( (sqrt(1.5^2 * (sind(x)).^2 -1 ).*((cosd(x)).^2 - (sind(x)).^2) - sind(x).*cosd(x).*...
+%                ( (1.5^2 * sind(x).*cosd(x))./sqrt(1.5^2 * (sind(x)).^2 -1 ) ) )./ (1.5^2 * (sind(x)).^2 -1) ) ) )  );
+% 
+% d2fTM = @(x) -2*cosd(x)-(2/pi)*( ( ( -(1.5^2)* (  ((2*sind(x))./(cosd(x)).^2).*(1.5^2*(sind(x)).^2 - 1)  + ...
+%             (2./(cosd(x)).^2).* (1.5^2*sind(x).*cosd(x)) )    )./ ((  ((1.5./cosd(x)).^2).*(1.5^2*(sind(x)).^2 - 1 ) + 1  ).^2) ) .* ...
+%              ( (1.5) * ( (sind(x)./ (cos(x)).^2 )  .* sqrt(1.5^2 * (sind(x)).^2 - 1) + (1./cosd(x)).*(1.5^2 * ...
+%             sind(x).*cosd(x))./sqrt(1.5^2 * (sind(x)).^2 -1 ) ) ) + (1./( ((1.5)^2./((cosd(x)).^2) ) .* (1.5^2 * (sind(x)).^2 - 1) + 1 ) ).* ...
+%             (  (1.5)* ( (( (cosd(x)).^3 +2*(sind(x)).^2 .*cosd(x) )./ ( (cosd(x)).^4 ) ).* sqrt(1.5^2 * (sind(x)).^2 - 1) + ... 
+%                ( sind(x)./( (cosd(x)).^2 )  ).* ( (1.5^2 * sind(x).*cosd(x))./sqrt(1.5^2 * (sind(x)).^2 -1 ) ) )  +  (1.5^3) * ( ...
+%                ( sind(x)./( (cosd(x)).^2 )  ).* ( (sind(x).*cosd(x))./sqrt(1.5^2 * (sind(x)).^2 -1 ) ) + (1./cosd(x)).* ...
+%                ( (sqrt(1.5^2 * (sind(x)).^2 -1 ).*((cosd(x)).^2 - (sind(x)).^2) - sind(x).*cosd(x).*...
+%                ( (1.5^2 * sind(x).*cosd(x))./sqrt(1.5^2 * (sind(x)).^2 -1 ) ) )./ (1.5^2 * (sind(x)).^2 -1) ) ) )  );
+
 f0TE = @(x) fTE(x,0);
 f1TE = @(x) fTE(x,1);
 
@@ -170,9 +189,39 @@ f1TE = @(x) fTE(x,1);
 f0TM = @(x) fTM(x,0);
 f1TM = @(x) fTM(x,1);
 
-%% Para m = 0, usamos rutina newton para hallar el modo con un error de 10^-3
+%% Para m = 0, usamos rutina newton para hallar el modo con un error de 10^-4
 % debido a la presición que se está manejando en el intervalo.
 
-[p0TE, err0TE, k0TE, y0TE] = newton(f0TE, dfTE, 64, 1E-10, 1E-3, 10000000);
+[p0TE, err0TE, k0TE, y0TE] = newton(f0TE, dfTE, 70, 1E-3, 1E-4, 100000);
+[p0TM, err0TM, k0TM, y0TM] = newton(f0TM, dfTM, 65, 1E-3, 1E-4, 100000);
+
+%% Para m = 1, usamos rutina newton para hallar el modo con un error de 10^-4
+% debido a la presición que se está manejando en el intervalo.
+
+[p1TE, err1TE, k1TE, y1TE] = newton(f1TE, dfTE, 49, 1E-3, 1E-4, 100000);
+[p1TM, err1TM, k1TM, y1TM] = newton(f1TM, dfTM, 44, 1E-3, 1E-4, 100000);
 
 
+%% Ya se encontraron los angulos de incidencia de cada modo y la cantidad de
+% modos , solo falta hallar el beta de cada uno y el indice efectivo, por
+% lo que se hara una tabla en la cual se mostraran los datos.
+
+modo_TE = [0;1];
+Beta_TE = [ ((2*pi)/1e-6) *1.5* sind(p0TE); ((2*pi)/1e-6) *1.5* sind(p1TE)];
+n_Efectivo_TE = [1.5* sind(p0TE); 1.5* sind(p1TE)];
+Angulo_TE = [p0TE;p1TE];
+
+% Create the table for TE modes
+TTE = table(modo_TE, Beta_TE, n_Efectivo_TE,Angulo_TE, 'VariableNames', {'Modo', 'Beta', 'Índice efectivo','Ángulo de incidencia'});
+disp("/////////////////// Descripción modos TE ////////////////////////")
+disp(TTE);
+
+modo_TM = [0;1];
+Beta_TM = [ ((2*pi)/1e-6) *1.5* sind(p0TM); ((2*pi)/1e-6) *1.5* sind(p1TM)];
+n_Efectivo_TM = [1.5* sind(p0TM); 1.5* sind(p1TM)];
+Angulo_TM = [p0TM;p1TM];
+
+% Create the table for TM modes
+TTM = table(modo_TM, Beta_TM, n_Efectivo_TM,Angulo_TM, 'VariableNames', {'Modo', 'Beta', 'Índice efectivo','Ángulo de incidencia'});
+disp("/////////////////// Descripción modos TM ////////////////////////")
+disp(TTM);
