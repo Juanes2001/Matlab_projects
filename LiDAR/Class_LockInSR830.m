7496629f8a8985d596ddec385eebe1c7e0773ac8
Juanes2001 <jrodriguezoc@unal.edu.co>
2025-06-09 18:42:35 -0500

    c

---
+++ LiDAR/Class_LockInSR830.m
@@ -0,0 +1,20 @@
+classdef Class_LockInSR830
+    
+    properties
+        
+    
+    
+    end
+    
+    methods
+    
+        function obj = Class_LockInSR830(port, baudrate)
+                
+
+        end
+
+
+
+    end
+end
+
